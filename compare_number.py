cadabf9bc66a3ed4b061c003fa5ecaca5868b78a
juyeongpark <juyeongpark@coredax.com>
2022-12-30 11:01:02 +0900

    [programmers] 숫자 비교하기

---
+++ compare_number.py
@@ -0,0 +1,25 @@
+
+def solution(num1, num2):
+    '''
+    문제: 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+    출처: https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=python3
+    '''
+
+    if (num1 == num2) :
+        return 1
+    else: 
+        return -1
+    
+    
+    
+def solution2(num1, num2):
+    if num == num 2: 
+        answer = 1
+    else:
+        answer = -1
+    return answer
+
+
+
+def solution3 (num1, num2):
+    return 1 if num1 == num2 else -1 
