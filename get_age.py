2e260a9ea16b6358a1b1268b6e6982984aef8be6
juyeong-repo <juyeong.park.tech@gmail.com>
2022-12-31 16:54:36 +0900

    [programmers] 나이 출력

---
+++ get_age.py
@@ -0,0 +1,15 @@
+def solution (age) :
+    '''
+    문제 : 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+    출처 : https://school.programmers.co.kr/learn/courses/30/lessons/120820
+    suedo code : 2022년 - age (integer) + 1 을 리턴
+    '''
+    answer = (2022 - age ) +1 
+    return answer
+
+
+
+def solution (age) :
+    return  2023 - age
+
+    
