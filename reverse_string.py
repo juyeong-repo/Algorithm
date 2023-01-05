ed67ea1c9907cdf89cd49f6e80c2cd075fc73434
juyeong-repo <juyeong.park.tech@gmail.com>
2023-01-05 23:54:55 +0900

    [programmers] 문자열 뒤집기

---
+++ reverse_string.py
@@ -0,0 +1,18 @@
+def solution(my_string):
+    '''
+    question:
+    문자열 my_string이 매개변수로 주어집니다. 
+    my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+    source: https://school.programmers.co.kr/learn/courses/30/lessons/120822
+    suedo code:
+    1. numpy array 를 뒤집기
+    2. 리스트 슬라이싱 사용 
+    '''
+    answer = my_string[::-1]
+    return answer
+
+
+    
+
+
+
