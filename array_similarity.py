99c6c2df069bf73aec8ff6145731b0893d401a1b
juyeong-repo <juyeong.park.tech@gmail.com>
2023-01-09 00:00:05 +0900

    array_similarity

--- array_similarity.py
+++ array_similarity.py
@@ -1,2 +1,11 @@
 def solution (s1,s2):
     return len(s1) + len(s2) - len(set([s1+s2]))
+
+
+def solution (s1,s2):
+    answer = 0
+    for i in s1:
+        if i in s2:
+            answer += 1
+            
+
