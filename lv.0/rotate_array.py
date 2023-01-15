911f8a277d59b2817b6491eb0c23e3d44bbb66d4
juyeong-repo <juyeong.park.tech@gmail.com>
2023-01-15 12:14:08 +0900

    [programmers] 배열 회전시키기: deque 사용한 솔루션 추가

--- lv.0/rotate_array.py
+++ lv.0/rotate_array.py
@@ -1,14 +1,28 @@
-def solution(numbers, direction):
+from collections import deque
+
+
+def solution1(numbers, direction):
     '''
-    source:
-    suedo code:
-    direction이 left, right인 경우: if문을 사용하여 처리->
-        파이썬에서 리스트 원소 순서 변환하는 방법 사용 : list slice (solution1)
-        배열 안을 순회하여 순서 변경할 때 : collection lib의 collections.deque 사용 (solutio         n2)
-    
+    suedo code: 
+    1) when direction equals left or right
+    2) use python slicing
+    이렇게 풀 수 있고 (solution1) 
+    파이썬 컬렉션의 deque를 사용해도 된다.  (solution2)
     '''
     if direction == 'right':
         return [numbers[-1]] + numbers[:-1]
     else:
         return numbers[1:] + [numbers[0]]
     return answer
+
+
+def solution2 (numbers,direction):
+    if direction == 'right':
+        numbers = deque(numbers)
+        numbers.rotate(1)
+        return list(numbers)
+    else:
+        numbers = deque(numbers)
+        numbers.rotate(-1)
+        return list(numbers)
+    
