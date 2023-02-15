278030a6829b7a7b5323b39fc69e811d0c89e88a
juyeong-repo <juyeong.park.tech@gmail.com>
2023-02-15 10:05:06 +0900

    [programmers] 문자열 정렬하기

--- lv.0/push_string.py
+++ lv.0/push_string.py
@@ -1,14 +1,12 @@
-def solution(A, B):
-    '''
-    source: 
-    idea: 
-    문자열을 미는 거니까, 처음엔 deque가 문자열을 특정 방향으로 돈다면? 이란 생각을했다.
-    그 다음은 인덱스 슬라이싱
-    '''
-    A = 'hello'
-    B = 'ohell'
-    str1 = list(A)
-    str2 = list(B)
-    if str1[1:-1]+str1[0] == str2:
-        print ('1')
-    print ('-1')
+from collections import deque
+
+def solution(A,B):
+    A, B = deque(A), deque(B)
+    
+    for cnt in range(len(A)):
+        if A == B:
+            return cnt
+        
+        A.rotate()
+    
+    return -1
