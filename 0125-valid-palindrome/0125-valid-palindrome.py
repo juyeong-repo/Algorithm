4537f2d0f4a7deff4e7ca278304348e939a08783
juyeong-repo <77119824+juyeong-repo@users.noreply.github.com>
2023-02-20 16:52:45 +0900

    Time: 43 ms (84.76%), Space: 15.6 MB (16.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-z0-9]','',s.lower())
-        return s == ''.join(reversed(s))
+        s = s.lower()
+        s = re.sub('[^a-z0-9]','',s)
+        return s == s [::-1]
