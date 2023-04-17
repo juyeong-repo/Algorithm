1979acfbc1d174b051b4060a3926283fe988d680
juyeong-repo <77119824+juyeong-repo@users.noreply.github.com>
2023-04-17 21:26:57 +0900

    Time: 327 ms (41.35%), Space: 16.5 MB (48.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,8 @@
 class Solution:
 
+    dx = [0,1,0,-1]
+    dy = [-1,0,1,0]
+    
     def numIslands(self, grid: List[List[str]]) -> int:
         
         cnt = 0
@@ -12,21 +15,14 @@ class Solution:
 
         return cnt
             
-    
     def dfs(self, grid, i, j) :
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+        if i<0 or i>= len(grid) or j<0 or j>= len(grid[0]) or grid[i][j] == '0':
             return
         
         grid[i][j] = '0'
         
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
-    
-    
-    
-    
+        for k in range(4):
+            self.dfs(grid, i+self.dx[k], j+self.dy[k])
     
     
         
