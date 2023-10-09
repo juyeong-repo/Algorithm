73b9e86e6f228bb1433135f88347e30aafeffeba
juyeong-repo <77119824+juyeong-repo@users.noreply.github.com>
2023-10-09 19:35:06 +0900

    Time: 45 ms (81.33%), Space: 18 MB (19.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,3 +6,24 @@ class Solution:
         return s == s[::-1]
         
         
+        
+
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+        
+        #1. 데이터 전처리 -> ['a','m', .. '[a]']
+        s = [] 
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+                
+        #2. 팰린드롬 여부 판별
+        while len(strs) > 1: # 예외처리
+            if strs.pop(0) != strs.pop():
+                return False
+            # 모두 통과했다면 True 리턴
+            
+    
+            
+                
+        
