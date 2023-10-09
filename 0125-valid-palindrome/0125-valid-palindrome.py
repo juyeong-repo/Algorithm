4c92c844e94f981ab7965e62f224967634046fb6
juyeong-repo <77119824+juyeong-repo@users.noreply.github.com>
2023-10-09 19:47:51 +0900

    Time: 55 ms (34.28%), Space: 21.8 MB (13.40%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution3:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         s = re.sub ('[^a-z0-9]','',s)
@@ -24,6 +24,22 @@ class Solution1:
             # 모두 통과했다면 True 리턴
             
     
+
+    
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # 자료형 데크로 선언
+        strs:Deque = collections.deque()
             
+        for char in s: 
+            if char.isalnum():
+                strs.append(char.lower())
+                
+        while len(strs) > 1: 
+            if strs.popleft() != strs.pop():
+                return False
+            
+        return True
                 
         
