112a75f8ecc9067ddc1168e2e8c5472bd6e7ec99
juyeong-repo <77119824+juyeong-repo@users.noreply.github.com>
2023-10-22 20:34:41 +0900

    Time: 371 ms (40.67%), Space: 17.1 MB (82.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # dictionary initialize
-        nums_map = {}
-        
-        # key와 value를 바꿔서 dictionary로 저장
-        for i, num in enumerate(nums):
-            # 원소가 key값이 된다 
-            nums_map[num] = i
-        
-        # target에서 첫 번째 수를 뺀 결과를 key로 조회
-        for i, num in enumerate(nums):
-            if target - num in nums_map and i != nums_map[target - num]:
-                return [i, nums_map[target - num]]
-            
-            
-        nums_map = {}
+        for i, n in enumerate(nums) :
+            compliment = target - n
+
+            if compliment in nums[i+1:]:
+                return [nums.index(n),nums[i+1:].index(compliment) + (i+1)]
